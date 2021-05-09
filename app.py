eab98ca65fb53fe8d6856f8a3d9cb1a2f7294e8a
Fajar Garnadi <fajar.garnadi@gmail.com>
2021-05-10 06:23:51 +0700

    Add noop routes

--- app.py
+++ app.py
@@ -5,5 +5,15 @@ app = Flask(__name__)
 def hello_world():
     return 'Hello World!'
 
+
+@app.route('/register', methods=['POST'])
+def register():
+    pass
+
+
+@app.route('/register/<string:service>/routes', methods=['POST'])
+def register_routes(service):
+    pass
+
 if __name__ == '__main__':
     app.run()
